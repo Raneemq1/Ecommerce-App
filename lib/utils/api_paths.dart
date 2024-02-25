8dd90d86dbb0af9f8ad451d0785768f1c626c204
Raneemq1 <raneemalqadi32@gmail.com>
2024-02-26 00:22:57 +0200

    add firestore service file and api paths

---
+++ lib/utils/api_paths.dart
@@ -0,0 +1,6 @@
+class ApiPaths {
+  static String getProduct(String id) => 'products/$id';
+  static String getUser(String id) => 'users/$id';
+  static String getAds(String id) => 'ads/$id';
+  static String getCategories(String id) => 'categories/$id';
+}
--- lib/views/pages/register_page.dart
+++ lib/views/pages/register_page.dart
@@ -1,49 +1,45 @@
 import 'package:ecommerce_app/utils/app_colors.dart';
-import 'package:ecommerce_app/viewmodel/cubit/auth/auth_cubit.dart';
 import 'package:ecommerce_app/views/widgets/register_form.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(),
-      child: Scaffold(
-          body: SafeArea(
-              child: Padding(
-        padding: EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Create Account',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-              const SizedBox(
-                height: 4,
-              ),
-              Text(
-                'Let\'s create your account',
-                style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                      color: AppColors.grey,
-                      fontSize: 12,
-                    ),
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              RegisterForm(),
-            ],
-          ),
+    return Scaffold(
+        body: SafeArea(
+            child: Padding(
+      padding: EdgeInsets.all(20),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Create Account',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Let\'s create your account',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: AppColors.grey,
+                    fontSize: 12,
+                  ),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            RegisterForm(),
+          ],
         ),
-      ))),
-    );
+      ),
+    )));
   }
 }
